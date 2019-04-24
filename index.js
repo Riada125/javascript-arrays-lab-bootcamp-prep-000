53400404e7fcb7bac52f14ddb03f803d560d00cf
Michael Adair <riada125@hotmail.com>
2019-04-24 22:21:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -39,4 +39,7 @@ function removeLastKitten(name){
 var newKittens = kittens.slice(0, kittens.length - 1)
   return newKittens;
 }
-
+var kittens = ["Milo","Otis","Garfield"]
+function removeFirstKitten(name){
+var newKittens = kittens.slice(0, kittens.length - 1)
+  return newKittens;
